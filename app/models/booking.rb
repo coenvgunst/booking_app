b31da718c9cad6336a03d5d1821bea2b7e9f070b
Coen van Gunst <coendokkum@hotmail.nl>
2024-09-09 10:27:16 +0200

    updated homepage & flow

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,15 @@ class Booking < ApplicationRecord
   has_many :booking_treatments, dependent: :destroy
   has_many :treatments, through: :booking_treatments
   has_one :calendar, dependent: :destroy
+
+  validate :must_have_at_least_one_treatment
+
+  private
+
+  # Validates if there is at least one treatment selected
+  def must_have_at_least_one_treatment
+    if treatments.empty?
+      errors.add(:treatments, "must include at least one treatment")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'home#index'
   
+  root 'home#index'
+
   resources :calendars
   resources :booking_treatments
   resources :bookings
   resources :treatments
   resources :customers
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
